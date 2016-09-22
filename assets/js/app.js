ca859b8e370582ec87eb799439ee5ffd121ce771
Meenakshi Sundaram <vms20591@gmail.com>
2016-09-22 22:23:14 +0530

    Add back to top feature and change navbar background color on scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
   var nav=$("nav#navbar");
   var $window=$(window);
+  var backToTop=$("div.back-to-top");
   
   $window.scroll(function(event){
+    backToTop.css('display','block');
+
     if(!nav.hasClass('box-shadow')){
       nav.addClass('box-shadow');
     }
@@ -13,6 +16,15 @@ $(document).ready(function(){
     if($window.scrollTop()===0){
       nav.removeClass('box-shadow');
       nav.removeClass('fixed-nav');
+      backToTop.css('display','none');
     }
+  });
+
+  backToTop.click(function(){
+    $('html,body').animate(
+      {
+        scrollTop:'0'
+      },
+    800);
   });
 }); 
